be932b5b2035595fc188e409d6492b24418fb433
Deividas Ovsianikovas <deividov2000@gmail.com>
2021-03-06 23:57:55 +0000

    Installed Location Libs, added some sample UI

--- SafeAmigosApp/App.js
+++ SafeAmigosApp/App.js
@@ -4,48 +4,55 @@
  * @flow strict-local
  */
 
-
+import React, { useState, useEffect } from 'react';
 ///import libs
-import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
+  Button,
+  Alert, 
 } from 'react-native';
 
 
-//imports generic screen for react 
-import {
-  Header,
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
-
-
-/*geolocation lib
-Install these if nto working
-https://github.com/expo/expo
-https://github.com/expo/expo/tree/master/packages/expo-location
-*/
-//import * as Location from 'expo-location';
-
+/*geolocation lib*/
+import * as Location from 'expo-location';
 
 
 ///This gets called as soon as the app is opened / updated
 const App: () => React$Node = () => {
 
+
+  //Sample code courtesy of https://docs.expo.io/versions/latest/sdk/location/
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let userLocation = 'Waiting..';
+  if (errorMsg) {
+    userLocation = errorMsg;
+  } else if (location) {
+    userLocation = JSON.stringify(location);
+  }
+
   //Can call methods soon as app is opened to test, or call from UI inputs
   ConsoleTest();
 
   ///print out simple success message on app load, shows in vscode output
-  function ConsoleTest()
-  {
-    console.log("App updated successfully!");
+  function ConsoleTest() {
+    console.log("App updated successfully! Users location: " + userLocation);
   }
-
-  ///location method, from https://docs.expo.io/versions/latest/sdk/location/
 
 
 
@@ -53,30 +60,32 @@ const App: () => React$Node = () => {
   //Can create methods to send data to nodejs & add to firebase db here
 
 
+
+
   ///The markup of the app, similar to how HTML works
+  ///Note how the curly braces look when setting styling
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>The Amigos</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+      <View style = {styles.View}>
+        <Text style = {{fontSize: 30}}>
+          Lil Text
+        </Text>
+        <Button 
+          title = "SOMETHING"
+          color = ""
+          onPress={() => Alert.alert('Something more')}
+        />
+
+        <Text style={styles.someTextStyle}>
+          Another Example
+        </Text>
+
+        <Text>
+          Your location + {userLocation}
+        </Text>
+
+      </View>
+
     </>
   );
 };
@@ -84,41 +93,13 @@ const App: () => React$Node = () => {
 
 //The styling for above components, similar to way css works
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  someTextStyle: {
+    color: "red",
+    marginTop: 20,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+
+  View: {
+    margin: 30,
   },
 });
 
--- SafeAmigosApp/android/app/src/main/java/com/safeamigosapp/MainApplication.java
+++ SafeAmigosApp/android/app/src/main/java/com/safeamigosapp/MainApplication.java
@@ -1,6 +1,10 @@
 package com.safeamigosapp;
 
 import android.app.Application;
+
+// com.myapp should be your package name
+import com.safeamigosapp.generated.BasePackageList;
+
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
@@ -10,8 +14,15 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import java.util.Arrays;
+ 
+import org.unimodules.adapters.react.ModuleRegistryAdapter;
+import org.unimodules.adapters.react.ReactModuleRegistryProvider;
+import org.unimodules.core.interfaces.SingletonModule;
+
 
 public class MainApplication extends Application implements ReactApplication {
+  private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(new BasePackageList().getPackageList(), null);
 
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
@@ -26,6 +37,11 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+              // Add unimodules
+              List<ReactPackage> unimodules = Arrays.<ReactPackage>asList(
+                new ModuleRegistryAdapter(mModuleRegistryProvider)
+              );
+              packages.addAll(unimodules);
           return packages;
         }
 
