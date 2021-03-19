ebaba4f4ad40e0d27c45c011c0308836608e8727
Elliott McStay <elliott.mcstay@gmail.com>
2021-03-19 13:28:56 +0000

    Receiving a call

--- BackEnd/functions/index.js
+++ BackEnd/functions/index.js
@@ -9,3 +9,24 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
     functions.logger.info("Hello logs!", { structuredData: true });
     response.send("Hello from Firebase!");
 });
+
+
+//defining credentials
+var accountSid = "AC79b407975ae9f0b4d19af767e5da6b77";
+var authToken = "ea8845abc935e2fbfcce8ee4e18b8994";
+
+var twilio = require('twilio');
+var client = require('twilio')(accountSid, authToken); //defining client variable
+
+//create outbound call
+client.calls.create({
+    url: 'http://demo.twilio.com/docs/voice.xml', //instructions fro when call connects
+    to: '+353861054656', //who is receiving 
+    from: '+353862246656' //where call is coming from
+}, function(err, call) {
+    if (err) {
+        console.log(err); //if error, log error in console
+    } else {
+        console.log(call.sid) //else, log completed call
+    }
+})
