f4a1992c57d092677f36b8151a31e930cd966c80
Deividas Ovsianikovas <deividov2000@gmail.com>
2021-04-02 21:04:44 +0100

    Reverted App.js
    Previous commit deleted a lot of files

--- SafeAmigosApp/App.js
+++ SafeAmigosApp/App.js
@@ -11,20 +11,38 @@ import {
   View,
   Text,
   Button,
-  Alert, 
-  AppRegistry
+  Alert,
+  AppRegistry,
+  TextInput
 } from 'react-native';
 
 
 /*geolocation lib*/
+//import database from '@react-native-firebase/database';
+//import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+//import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import * as Location from 'expo-location';
 import { helloWorld } from '../BackEnd/functions';
 
+function addContact (name, number ) {
+  const newReference = firebase.database()
+  .ref('/users')
+  .push();
 
-AppRegistry.registerComponent('main',() => App);
+console.log('Auto generated key: ', newReference.key);
+
+newReference
+  .set({
+    full_name: name,
+    phone_number: number,
+  })
+  .then(() => console.log('Data updated.'));
+}
+
 
 ///This gets called as soon as the app is opened / updated
 const App: () => ReactNode = () => {
+
 
 
   //Sample code courtesy of https://docs.expo.io/versions/latest/sdk/location/
@@ -55,7 +73,7 @@ const App: () => ReactNode = () => {
 
   ///print out simple success message on app load, shows in vscode output
   function ConsoleTest() {
-    console.log("App updated successfully! Users location: " + userLocation);
+    console.log("App updated successfully! Users location: \n" + userLocation + "\n");
   }
 
 
@@ -69,48 +87,81 @@ const App: () => ReactNode = () => {
 
   }
 
+  const [name, setName] = React.useState('');
+  const [number, setNumber] = React.useState('');
 
-
-
-  ///The markup of the app, similar to how HTML works
-  ///Note how the curly braces look when setting styling
   return (
-    <>
-      <View style = {styles.View}>
-        <Text style = {{fontSize: 30}}>
-          Lil Text
-        </Text>
-        <Button 
-          title = "SOMETHING"
-          color = ""
-          onPress={() => Alert.alert('Something more')}
-        />
+    <View>
+      
+        <Text style={styles.header}>Create New Contact</Text>
+      
+      <Text style={styles.textStyle}>Name:</Text>
+      <TextInput
+         style={styles.input}
+         onChangeText={setName}
+         //value={text}
+         placeholder="John Smith"
+         autoCorrect={false}
+         maxLength={40}
+         //onChangeText={name => this.setName({name})}
+    />
+        <Text style={styles.textStyle}>Phone Number:</Text>
+        <TextInput
+         style={styles.input}
+         onChangeText={setNumber}
+            value={number}
+            placeholder="0861234567"
+            keyboardType="numeric"
+            maxLength={10}
+           // onChangeText={(text) => this.setNumber({number:text})}
+    />
+       
+       <View style={{margin:20, borderRadius: 10, borderWidth: 2}}>
+        <Button
+          title="Add contact"
+          color="black"
+          onPress={() => {
+            addContact(name, number);
+          }}
+          />  
+        </View>
 
-        <Text style={styles.someTextStyle}>
-          Another Example
-        </Text>
-
-        <Text>
-          Your location + {userLocation}
-        </Text>
-
-      </View>
-
-    </>
-  );
+      <View style={{margin:20, borderRadius: 10, borderWidth: 2}}>
+        <Button
+          title="Cancel"
+          color="black"
+          />  
+        </View>
+               
+    </View>
+);
 };
 
-
-//The styling for above components, similar to way css works
 const styles = StyleSheet.create({
-  someTextStyle: {
-    color: "red",
-    marginTop: 20,
-  },
-
-  View: {
-    margin: 30,
-  },
+header:{
+  textAlign: "center",
+  fontSize: 50,
+  marginTop: 50
+},
+input:{
+  fontSize: 25,
+  marginLeft: 10,
+  padding: 4,
+  borderWidth: 1,
+  borderColor: "#20232a",
+},
+textStyle: {
+  fontSize: 25,
+  marginLeft: 10,
+  padding: 10,
+}
 });
 
+
 export default App;
+
+/*
+
+<Text>
+Your location + {userLocation}
+</Text>*/
