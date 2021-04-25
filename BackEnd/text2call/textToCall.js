363c4c886f5eecb0d48be6eadbd3f59f657c297a
Elliott McStay <elliott.mcstay@gmail.com>
2021-04-25 13:43:59 +0100

    Update textToCall.js

--- BackEnd/text2call/textToCall.js
+++ BackEnd/text2call/textToCall.js
@@ -1,17 +1,21 @@
+//initialising consts accountSID and authenticationToken to determine which project an API request is coming from
 const accountSID = 'AC79b407975ae9f0b4d19af767e5da6b77';
 const authenticationToken = 'ea8845abc935e2fbfcce8ee4e18b8994';
 const client = require('twilio')(accountSID, authenticationToken);
-const sender = '+19513632916';
+const sender = '+19513632916'; //using a twilio-purchased number to test with
 
+//send a new outgoing message from a Twilio phone number
 client.messages.create({
-    to: '+353861054656',
-    from: sender,
-    body: 'testUser is in trouble! Enter a call with' + ' ' + sender + ' ' + 'to see if they are ok!'
+        to: '+353861054656', //test number, this parameter determines the destination phone number for the SMS message
+        from: sender, //this parameter determines the sender of the SMS message
+        body: 'testUser is in trouble! Enter a call with' + ' ' + sender + ' ' + 'to see if they are ok!' //this parametr determines the body of the message being sent
 
-}, function(err, message) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(message.sid)
-    }
-})
+    },
+    //if there's an error, writes a message to log on the debugging console
+    function(err, message) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(message.sid)
+        }
+    })
