956d0914a861aeff5ed30e2ad1dc403dbb7e1462
Padraig <padraig.halstead.2020@mumail.ie>
2021-05-04 12:35:04 +0100

    Update App.js

--- SafeAmigosApp/App.js
+++ SafeAmigosApp/App.js
@@ -5,11 +5,32 @@
 
 import Controller from './screens/SampleAppScreen'
 
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './screens/HomeScreen';
+import ContactsScreen from './screens/CreateContactsScreen';
+import CreateContactsScreen from './screens/CreateEmergencyContactScreen';
+import CreateUserScreen from './screens/CreateUserScreen';
 
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    CreateContact: CreateEmergencyContactScreen,
+    CreateUser: CreateUserScreen,
+    Location: LocationScreen
+  },
+  {
+    initialRouteName: 'HomeScreen',
+    defaultNavigationOptions: {
+      title: 'App',
+    },
+  }
+);
 
 ///Called every time app is updated
 const App = () => {
   return (Controller.instance.screen1());
 };
 
+export default createAppContainer(navigator);
 export default App;
